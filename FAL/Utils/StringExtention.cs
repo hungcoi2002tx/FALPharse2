a869e518cc487aead97a27e27d4db454a230df87
Nguyen Minh Hung <Hackinghungtx@gmail.com>
2024-10-11 00:17:34 +0700

    fix khanm

---
+++ FAL/Utils/StringExtention.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Newtonsoft.Json.Linq;
+using Share.SystemModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FAL.Utils
+{
+    public class StringExtention
+    {
+        public static string GetKeyByJson(string json)
+        {
+			try
+			{
+                JObject jsonObject = JObject.Parse(json);
+                return jsonObject[GlobalVarians.SystermId].ToString();
+            }
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
