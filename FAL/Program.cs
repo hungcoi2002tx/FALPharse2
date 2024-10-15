4bfb053bd11e7f72a7892857b6ea2dbac8365d04
KhaNM <kha.minh1452@gmail.com>
2024-10-16 05:56:52 +0700

    Use swagger for product enviroment

--- FAL/Program.cs
+++ FAL/Program.cs
@@ -112,12 +112,14 @@ namespace FAL
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
             // Use swagger for product enviroment  
             app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
