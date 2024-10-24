b0b173421f7f4f33d1bf8e6f87be2d38f9762c63
thaiLA <leanthai02@gmail.com>
2024-10-25 00:59:03 +0700

    compare face

--- TakePictureDemo/Form1.Designer.cs
+++ TakePictureDemo/Form1.Designer.cs
@@ -28,19 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            sendBtn = new Button();
+            txtBoxToken = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtResult = new TextBox();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(380, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 23);
+            button1.TabIndex = 0;
+            button1.Text = "choose picture";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 115);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(220, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // sendBtn
+            // 
+            sendBtn.Location = new Point(252, 258);
+            sendBtn.Name = "sendBtn";
+            sendBtn.Size = new Size(160, 23);
+            sendBtn.TabIndex = 2;
+            sendBtn.Text = "send";
+            sendBtn.UseVisualStyleBackColor = true;
+            sendBtn.Click += sendBtn_Click;
+            // 
+            // txtBoxToken
+            // 
+            txtBoxToken.Location = new Point(154, 61);
+            txtBoxToken.Name = "txtBoxToken";
+            txtBoxToken.Size = new Size(220, 23);
+            txtBoxToken.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(154, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Token";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(154, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 15);
+            label2.TabIndex = 5;
+            label2.Text = "picture source path";
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(154, 170);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(217, 23);
+            txtResult.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(154, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 7;
+            label3.Text = "result";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(label3);
+            Controls.Add(txtResult);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtBoxToken);
+            Controls.Add(sendBtn);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button sendBtn;
+        private TextBox txtBoxToken;
+        private Label label1;
+        private Label label2;
+        private TextBox txtResult;
+        private Label label3;
     }
 }
--- TakePictureDemo/Form1.cs
+++ TakePictureDemo/Form1.cs
@@ -2,9 +2,11 @@
 using System.Drawing;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace TakePictureDemo
 {
@@ -12,6 +14,8 @@ namespace TakePictureDemo
     {
         private FilterInfoCollection videoDevices;
         private VideoCaptureDevice videoSource;
+        private string captureImagePath;
+        private string targetImagePath;
 
         public Form1()
         {
@@ -47,36 +51,70 @@ namespace TakePictureDemo
             image.Save(path);
             // Dừng việc capture sau khi đã chụp ảnh
             videoSource.SignalToStop();
-
+            captureImagePath = path;
             MessageBox.Show($"Đã chụp ảnh và lưu vào {path}");
 
             // Gửi ảnh đến API
-            await SendImageToApi(path);
+
         }
 
-        private async Task SendImageToApi(string filePath)
+        private async Task SendImageToApi()
         {
             try
             {
                 var handler = new HttpClientHandler
-                  {
-                      ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
-                  };
-                using (HttpClient client = new HttpClient(handler))
                 {
-                    var request = new HttpRequestMessage(HttpMethod.Post, "http://fal-dev.eba-55qpmvbp.ap-southeast-1.elasticbeanstalk.com/api/Detect");
-                    request.Headers.Add("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJraGFubSIsInJvbGVJZCI6IjEiLCJzeXN0ZW1OYW1lIjoiZnVhbHVtbmkiLCJqdGkiOiIwYjM0MTQ5NS04ZmY1LTRjODQtYTk0Zi03Y2YyM2UzMWQ1NzMiLCJleHAiOjE3Mjk3MDIxMzQsImlzcyI6InlvdXJfaXNzdWVyIiwiYXVkIjoieW91cl9hdWRpZW5jZSJ9.rv9AKboWOGZbM1E4bbGQ7u6uzLAXe9Aaeq6QnTpx9To");
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                };
 
-                    // Chuẩn bị nội dung cho request
-                    var content = new MultipartFormDataContent();
-                    content.Add(new StreamContent(File.OpenRead(filePath)), "File", "capture.jpg");
-                    request.Content = content;
+                var apiUrl = "https://localhost:7065/api/Compare/compare";
+                var token = txtBoxToken.Text;
+                var sourceImagePath = captureImagePath;
+                var targetImagePath = textBox1.Text;
 
-                    // Gửi request và xử lý phản hồi
-                    var response = await client.SendAsync(request);
-                    response.EnsureSuccessStatusCode();
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    MessageBox.Show($"Kết quả từ server: {responseBody}");
+
+                using (var httpClient = new HttpClient(handler))
+                {
+                    // Nếu token không trống, thêm Authorization header
+                    if (!string.IsNullOrEmpty(token))
+                    {
+                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    }
+
+                    using (var form = new MultipartFormDataContent())
+                    {
+                        // Đọc nội dung ảnh
+                        var sourceImageContent = new ByteArrayContent(File.ReadAllBytes(sourceImagePath));
+                        var targetImageContent = new ByteArrayContent(File.ReadAllBytes(targetImagePath));
+
+                        // Thêm nội dung vào form data với tên của các phần tệp
+                        form.Add(sourceImageContent, "SourceImage", "source_image.jpg");
+                        form.Add(targetImageContent, "TargetImage", "target_image.jpg");
+
+                        try
+                        {
+                            // Gửi yêu cầu POST tới API
+                            var response = await httpClient.PostAsync(apiUrl, form);
+
+                            // Xử lý phản hồi
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var responseContent = await response.Content.ReadAsStringAsync();
+                                Console.WriteLine("Response: " + responseContent);
+
+                                txtResult.Text = responseContent;
+                            }
+                            else
+                            {
+                                txtResult.Text = response.StatusCode.ToString();
+                                Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Exception: {ex.Message}");
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -93,6 +131,31 @@ namespace TakePictureDemo
                 videoSource.WaitForStop();
             }
             base.OnFormClosing(e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Tạo OpenFileDialog để người dùng chọn tệp
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Thiết lập bộ lọc cho các loại tệp, ví dụ chỉ cho phép chọn ảnh
+                openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png|All files (*.*)|*.*";
+
+                // Hiển thị hộp thoại chọn tệp
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Lưu đường dẫn của tệp đã chọn
+                    string filePath = openFileDialog.FileName;
+
+                    // Hiển thị đường dẫn trong một TextBox hoặc Label (hoặc lưu ở nơi khác)
+                    textBox1.Text = filePath;  // Hiển thị đường dẫn trong TextBox
+                }
+            }
+        }
+
+        private async void sendBtn_Click(object sender, EventArgs e)
+        {
+            await SendImageToApi();
         }
     }
 }
