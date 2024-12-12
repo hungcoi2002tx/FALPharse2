c4756711b731ae2387911c702ed24fdf89fd6fa4
Nguyen Minh Hung <Hackinghungtx@gmail.com>
2024-12-12 14:52:45 +0700

    update generated dynamodb

---
+++ InitProject/Roles.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitProject
+{
+    [DynamoDBTable("Roles1")]
+    public class Role
+    {
+        [DynamoDBHashKey]
+        public int RoleId { get; set; }  // Partition Key
+        public string RoleName { get; set; }
+        public List<Permission> Permissions { get; set; }
+    }
+
+    public class Permission
+    {
+        public string Resource { get; set; }
+        public List<string> Actions { get; set; }
+    }
+}
