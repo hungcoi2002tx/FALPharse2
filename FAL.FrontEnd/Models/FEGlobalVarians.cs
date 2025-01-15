badc803ea7443936ecccd286a131dc28aef5eff5
Nguyen Minh Hung <Hackinghungtx@gmail.com>
2025-01-15 08:38:58 +0700

    Remove cloudwatc, addition condition create dynamodb

--- FAL.FrontEnd/Models/FEGlobalVarians.cs
+++ FAL.FrontEnd/Models/FEGlobalVarians.cs
@@ -3,7 +3,7 @@
     public static class FEGlobalVarians
     {
 
-        public static readonly string BE_URL = "https://dev.demorecognition.click";
+        public static readonly string BE_URL = "https://alumni.fpt.edu.vn/reg";
         //public static readonly string BE_URL = "https://localhost:7065";
 
         public static readonly string LOGIN_ENDPOINT = BE_URL + "/api/Auth/login";
